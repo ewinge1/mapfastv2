51dd3e03144e26ba8ad581ef43e6c8c506bf9fd7
ewinge1 <ericewin@usc.edu>
2021-05-05 13:03:27 -0700

    Folder setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A follow up work to MAPFAST with graph neural networks and a larger portfolio',
+    author='Eric Ewing',
+    license='MIT',
+)
